03fd04a792fe6097491be99f79685b680539b804
zakmoCA <zakeriyaamohamed@gmail.com>
2024-03-04 19:59:24 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+import random
+
+pygame.init()
+
+BLACK = (0, 0, 0)
+GREY = (128, 128, 128)
+YELLOW = (255, 255, 0)
+
+WIDTH, HEIGHT = 800, 800
+TILE_SIZE = 20
+GRID_WIDTH = WIDTH // TILE_SIZE
+GRID_HEIGHT = HEIGHT // TILE_SIZE
+FPS = 60
+
+# pygame methods: display, time, draw, event, QUIT, MOUSEBUTTONDOWN, mouse
+# KEYDOWN, K_SPACE, K_c, K_g
