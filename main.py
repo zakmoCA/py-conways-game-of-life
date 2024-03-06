8311a1ca0b9dd89ca65ab94815f05f6d1da66228
zakmoCA <zakeriyaamohamed@gmail.com>
2024-03-06 17:50:23 +1000

    cleaned up comments

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def main():
     update_freq = 120
     
     positions = set()
-    positions.add((10,10)) # will add tuple to positions set which we want, instead of two 10s individually if passed directly to positions
+    positions.add((10,10)) 
     while running:
         clock.tick(FPS)
         
@@ -76,9 +76,8 @@ def gen(num):
      
     
 def adjust_grid(positions):
-    # all_neighbours of all live cells from the current set of positions
     all_neighbours = set()
-    new_positions = set() # next frame - new set necessary so as to not mutate determinism of current states cells
+    new_positions = set() 
     
     for position in positions:
         neighbours = get_neighbours(position)
@@ -87,14 +86,14 @@ def adjust_grid(positions):
         neighbours = list(filter(lambda x: x in positions, neighbours))
         
         if len(neighbours) in [2, 3]:
-            new_positions.add(position) # cell is safe!
+            new_positions.add(position) 
             
     for position in all_neighbours: 
         neighbours = get_neighbours(position)
         neighbours = list(filter(lambda x: x in positions, neighbours))
         
         if len(neighbours) == 3:
-            new_positions.add(position) # cell has enough neighbours to come alive!
+            new_positions.add(position) 
     return new_positions
         
      
@@ -103,10 +102,10 @@ def get_neighbours(pos):
     x, y = pos
     neighbours = []
     for dx in [-1, 0, 1]: 
-        if x + dx < 0 or x + dx > GRID_WIDTH: # true means no displacement
-            continue # can continue as this means we are at current position
+        if x + dx < 0 or x + dx > GRID_WIDTH: 
+            continue
         for dy in [-1, 0, 1]:
-            if y + dy < 0 or y + dy > GRID_HEIGHT: # grid height/width as upper-bound so as to not go off the screen --> maybe implement expansion ability for grid (upper bound still necessary, but can be higher)
+            if y + dy < 0 or y + dy > GRID_HEIGHT: # upper-bound: maybe implement expansion ability for grid to look more like the real GoL
                 continue
             if dx == 0 and dy == 0:
                 continue
@@ -120,7 +119,7 @@ def draw_grid(positions):
         col, row = position
         top_left = (col * TILE_SIZE, row * TILE_SIZE)
         pygame.draw.rect(screen, YELLOW, (*top_left, TILE_SIZE, TILE_SIZE)) 
-        # *top_left: the prefixed * unpacks the tuple so that they're passed as individual args
+        # prefixed * unpacks the tuple so that they're passed as individual args
   
     for row in range(GRID_HEIGHT):
           pygame.draw.line(screen, BLACK, (0, row * TILE_SIZE), (WIDTH, row * TILE_SIZE))
